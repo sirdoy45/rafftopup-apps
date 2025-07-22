4b23425803e6826dc5ecb9f2b857e45256fec4bf
sirdoy45 <rahmanyodi14@gmail.com>
2025-07-22 15:27:43 +0700

    update typeseeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,12 +4,20 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Type;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
     public function run()
     {
-        Type::create(['name' => 'Game']);
-        Type::create(['name' => 'Pulsa']);
+        Type::create([
+            'name' => 'Game',
+            'slug' => Str::slug('Game')
+        ]);
+
+        Type::create([
+            'name' => 'Pulsa',
+            'slug' => Str::slug('Pulsa')
+        ]);
     }
 }
