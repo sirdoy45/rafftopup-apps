b13f24f48cc63a6d3b8bc9e96bd23745b2681607
sirdoy45 <rahmanyodi14@gmail.com>
2025-07-24 00:20:04 +0700

    midtrans

--- config/midtrans.php
+++ config/midtrans.php
@@ -3,7 +3,7 @@
 return [
     'serverKey' => env('MIDTRANS_SERVER_KEY'),
     'clientKey' => env('MIDTRANS_CLIENT_KEY'),
-    'isProduction' => env('MIDTRANS_IS_PRODUCTION', false),
+    'isProduction' => env('MIDTRANS_IS_PRODUCTION', true),
     'isSanitized' => true,
     'is3ds' => true,
 ];
