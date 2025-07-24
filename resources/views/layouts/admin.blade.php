f081972defa41a949dd91b233f12d522c1e80deb
sirdoy45 <rahmanyodi14@gmail.com>
2025-07-25 05:33:10 +0700

    hapus cart

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -96,7 +96,6 @@
             <!-- Mobile Menu -->
             <ul class="navbar-nav d-block d-lg-none mt-3">
               <li class="nav-item"><a class="nav-link" href="#">Hi, Admin</a></li>
-              <li class="nav-item"><a class="nav-link d-inline-block" href="#">Cart</a></li>
             </ul>
           </div>
         </nav>
