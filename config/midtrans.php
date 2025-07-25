03dca9872306749feee68c70405bbb9a4a3efa10
sirdoy45 <rahmanyodi14@gmail.com>
2025-07-25 20:36:21 +0700

    apimidtrans

--- config/midtrans.php
+++ config/midtrans.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'serverKey' => env('MIDTRANS_SERVER_KEY'),
-    'clientKey' => env('MIDTRANS_CLIENT_KEY'),
+    'serverKey' => 'Mid-server-9bOVBHQyRHdkoCize1eM31Ui',
+    'clientKey' => 'Mid-client-cHPEz8RpSXdcLD13',
     'isProduction' => env('MIDTRANS_IS_PRODUCTION', true),
     'isSanitized' => true,
     'is3ds' => true,
